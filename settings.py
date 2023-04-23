a3513e971208ecdaf8b36fbff6020a79be85eda7
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-04-23 19:13:19 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import pygame as pg
 class Settings():
 
     def __init__(self):
-        self.width = 1200
-        self.height = 800
-        self.load_image = pg.image.load("background.jpg")
+        self.width = 600
+        self.height = 400
+        self.load_image = pg.image.load("images/background.jpg")
