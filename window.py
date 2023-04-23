7dfc4519dd604919c7de04453c0cf392916b989b
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-04-23 17:06:37 +0300

    Изменение фона

--- window.py
+++ window.py
@@ -4,11 +4,14 @@ import pygame
 
 def run_game():
     pygame.init()
-    pygame.display.set_mode((600,400))
+    window =pygame.display.set_mode((600,400))
+    background = pygame.image.load("background.jpg")
+    background = pygame.transform.scale(background,(600,400))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+        window.blit(background,(0,0))
         pygame.display.flip()
 
 run_game()
