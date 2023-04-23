0826c1b45b080b8ab2595bae209a764a9fd17e84
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-04-23 21:48:27 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -3,7 +3,7 @@ class Ship():
     def __init__(self,ai_game):
         self.screen = ai_game
         self.screen_rect = ai_game.get_rect()
-        self.image = pg.image.load("images/ship.jpg")
+        self.image = pg.transform.scale(pg.image.load("images/ship.png"),(100,100))
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom 
     
