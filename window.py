aedfcb26db9bea67e1e5b95fcc35ef730af1d9ad
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-04-23 17:39:04 +0300

    Update window.py

--- window.py
+++ window.py
@@ -9,20 +9,16 @@ class AlienInvasion:
     def __init__(self):
         pg.init()
 
-        self.screen = pg.display.set_mode((1200, 800))
-        background = pg.image.load("background.jpg")
-        background = pg.transform.scale(background, (1200, 800))
+        self.screen = pg.display.set_mode((900, 600))
+        self.background = pg.transform.scale(pg.image.load("background.jpg"), (1200, 800))
         pg.display.set_caption("Alien Invasion")
 
     def run_game(self):
-        background = pg.image.load("background.jpg")
-        background = pg.transform.scale(background, (1200, 800))
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit()
-            self.screen.blit(background, (0, 0))
-
+            self.screen.blit(self.background, (0, 0))
             pg.display.flip()
 
 if __name__ == '__main__':
