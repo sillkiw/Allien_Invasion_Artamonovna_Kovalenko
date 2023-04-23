16a4c5aa745dce678f9d4b079efa87cb9a1ac601
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-04-23 19:13:26 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,6 +1,11 @@
 import pygame as pg
 class Ship():
     def __init__(self,ai_game):
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
-        self.image = pg.image.load("sd")
+        self.screen = ai_game
+        self.screen_rect = ai_game.get_rect()
+        self.image = pg.image.load("images/ship.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom 
+    
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
