ba958d729b0015eaef0751ca38d51f5672d6fda6
Artamonov Anfisa <123888607+anfisartamonova@users.noreply.github.com>
2023-04-23 17:27:26 +0300

    Update window.py

--- window.py
+++ window.py
@@ -1,17 +1,30 @@
-import pygame
+import sys
+import pygame as pg
 
 
 
-def run_game():
-    pygame.init()
-    window =pygame.display.set_mode((600,400))
-    background = pygame.image.load("background.jpg")
-    background = pygame.transform.scale(background,(600,400))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-        window.blit(background,(0,0))
-        pygame.display.flip()
 
-run_game()
+class AlienInvasion:
+
+    def __init__(self):
+        pg.init()
+
+        self.screen = pg.display.set_mode((1200, 800))
+        background = pg.image.load("background.jpg")
+        background = pg.transform.scale(background, (1200, 800))
+        pg.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        background = pg.image.load("background.jpg")
+        background = pg.transform.scale(background, (1200, 800))
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    sys.exit()
+            self.screen.blit(background, (0, 0))
+
+            pg.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
