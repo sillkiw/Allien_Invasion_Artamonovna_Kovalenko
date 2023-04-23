3f2c2d3c0ae39594f881f07278cb1cad4e5a1c2f
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-04-23 16:39:34 +0300

    Запуск окна

--- window.py
+++ window.py
@@ -1,4 +1,14 @@
 import pygame
 
-while True:
+
+
+def run_game():
+    pygame.init()
     pygame.display.set_mode((600,400))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+        pygame.display.flip()
+
+run_game()
