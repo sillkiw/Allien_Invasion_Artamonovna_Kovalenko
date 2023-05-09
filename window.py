bc3e9be7fc31bb7923b032e126c73223f24e3ead
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-09 15:10:21 +0300

    Update window.py

--- window.py
+++ window.py
@@ -14,7 +14,7 @@ class AlienInvasion:
         self.screen = pg.display.set_mode((0,0),pg.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        self.background = pg.transform.scale(self.settings.load_image, (self.settings.width, self.settings.height))
+        self.background = pg.transform.scale(self.settings.bg_image, (self.settings.screen_width, self.settings.screen_height))
 
         self.ship = Ship(self)
         pg.display.set_caption("Alien Invasion")
