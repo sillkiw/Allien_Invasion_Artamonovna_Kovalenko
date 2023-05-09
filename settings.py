f73bcc1589336cc24aea2660d59d2827224df1f2
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-09 11:16:55 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import pygame as pg
 class Settings():
 
     def __init__(self):
-        self.width = 600
-        self.height = 400
-        self.load_image = pg.image.load("images/background.jpg")
+        self.width = 1000
+        self.height = 600
+        self.load_image = pg.image.load("images/background.gif")
