21d017bd31842af9ff4cf2335fbeac4580d9df36
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-09 14:34:37 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,18 +1,26 @@
 import pygame as pg
 class Ship():
     def __init__(self,ai_game):
-        self.screen = ai_game
-        self.screen_rect = ai_game.get_rect()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = self.screen.get_rect()
+        
         self.image = pg.transform.scale(pg.image.load("images/ship.png").convert_alpha(),(100,100))
         self.rect = self.image.get_rect()
+    
         self.rect.midbottom = self.screen_rect.midbottom 
+         
+        self.x = float(self.rect.x)
+
+         #Флаги перемещения
         self.moving_right = False
         self.moving_left = False
     def update(self):
-        if self.moving_right:
-            self.rect.x +=1
-        if self.moving_left:
-            self.rect.x -=1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x +=self.settings.ship_speed
+        if self.moving_left and self.rect.left>0:
+            self.x -=self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
