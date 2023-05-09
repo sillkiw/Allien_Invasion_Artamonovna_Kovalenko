885990d914997b94372cd17387c5954f64f3a57d
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-09 15:10:16 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ import pygame as pg
 class Settings():
 
     def __init__(self):
-        self.width = 1000
-        self.height = 600
-        self.load_image = pg.image.load("images/background.gif")
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_image = pg.image.load("images/background.png")
+        self.shp_image = pg.image.load("images/ship.png")
         self.ship_speed = 1.5
