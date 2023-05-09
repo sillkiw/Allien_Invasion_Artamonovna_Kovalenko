ebffe368dc52617a1dd68367ad914785c16d096d
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-09 12:47:50 +0300

    Update window.py

--- window.py
+++ window.py
@@ -13,19 +13,38 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pg.display.set_mode((self.settings.width, self.settings.height))
         self.background = pg.transform.scale(self.settings.load_image, (self.settings.width, self.settings.height))
-      
+
         self.ship = Ship(self.screen)
         pg.display.set_caption("Alien Invasion")
 
     def run_game(self):
         while True:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
           
-            self.screen.blit(self.background, (0, 0))
-            self.ship.blitme()
-            pg.display.flip()
+
+    def _update_screen(self):
+        self.screen.blit(self.background, (0, 0))
+        self.ship.blitme()
+        pg.display.flip()
+
+    def _check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pg.K_LEFT:
+                    self.ship.moving_left = True
+            
+            
+            elif event.type == pg.KEYUP:
+                if event.key == pg.K_RIGHT:
+                    self.ship.moving_right=False
+                elif event.key == pg.K_LEFT:
+                    self.ship.moving_left = False
 
 if __name__ == '__main__':
     ai = AlienInvasion()
