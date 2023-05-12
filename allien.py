7b4a937304e399cb25d20eee85713a30ce650576
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-13 02:40:27 +0300

    Create allien.py

---
+++ allien.py
@@ -0,0 +1,15 @@
+import pygame as pg
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    def __init__(self,ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+
+        self.image = pg.image.load('images/aliensh.png')
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x) 
