329b47efda74b3314b0fd95a71efad6b4e72e247
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-12 21:38:02 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship():
         self.settings = ai_game.settings
         self.screen_rect = self.screen.get_rect()
         
-        self.image = pg.transform.scale(ai_game.settings.shp_image.convert_alpha(),(110,110))
+        self.image = pg.transform.scale(ai_game.settings.shp_image.convert_alpha(),(170,170))
         self.rect = self.image.get_rect()
     
         self.rect.midbottom = self.screen_rect.midbottom 
