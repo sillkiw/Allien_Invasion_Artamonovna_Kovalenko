af31b70b03a2596dd1b2de78a4e7dc7cc574903f
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-12 21:20:38 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings():
     def __init__(self):
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_image = pg.image.load("images/background.jpg")
+        self.bg_image = pg.image.load("images/background.png")
         self.shp_image = pg.image.load("images/ship.png")
         self.ship_speed = 1.5
