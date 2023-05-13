da89d749e0907c839fd1ca6393869e4c0d86ee23
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-13 14:06:34 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,4 +17,4 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (0,0,255)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 4
