73ac099a5053599adf7f0c2eb9e39e203c8082ad
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-13 03:18:19 +0300

    Update window.py

--- window.py
+++ window.py
@@ -43,11 +43,15 @@ class AlienInvasion:
         number_aliens_x = available_space_x // (2*alien_width)
 
         for alien_number in range(number_aliens_x):
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x 
-            self.aliens.add(alien)
-       
+           self._create_alien(alien_number)
+
+    def _create_alien(self,alien_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width+2*alien_width*alien_number 
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
+
     def _update_screen(self):
         self.screen.blit(self.background, (0, 0))
         self.ship.blitme()
