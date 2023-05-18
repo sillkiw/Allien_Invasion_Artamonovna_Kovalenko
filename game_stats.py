db59cf777f4fe6c596252bd9600ff211c7f9b42c
Artamonov Anfisa <123888607+anfisartamonova@users.noreply.github.com>
2023-05-19 00:01:33 +0300

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,14 @@ class GameStats():
         """Инициализация статистики"""
         self.settings = ai_game.settings
         self.reset_stats()
+
+        #запуск игры в неактивном состоянии
+        self.game_active = False
+        
         #флаг для завершения игры после потери последнего корабля
         self.game_active = True
+
+        
 
     def reset_stats(self):
         """Инициализация статистики, изменяющейся в ходе игры"""
