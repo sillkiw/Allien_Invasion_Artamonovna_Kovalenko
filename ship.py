47a6743027e3cca0490c85c1251e7e112b985954
Artamonov Anfisa <123888607+anfisartamonova@users.noreply.github.com>
2023-05-18 19:28:49 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,13 +1,18 @@
 import pygame as pg
 class Ship():
+    """Управление коряблем"""
     def __init__(self,ai_game):
+        
+        #создание корабля(корабль рассматривается как прямоугольник)
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = self.screen.get_rect()
-    
+        
+        #загрузка изображения корабля
         self.image = pg.transform.scale(ai_game.settings.shp_image.convert_alpha(),(90,90))
         self.rect = self.image.get_rect()
-    
+
+        #задание стартовой позитиции корабля(середина нижнего края экрана)
         self.rect.midbottom = self.screen_rect.midbottom
       
         self.x = float(self.rect.x)
@@ -15,7 +20,9 @@ class Ship():
          #Флаги перемещения
         self.moving_right = False
         self.moving_left = False
+    
     def update(self):
+        """Ограничение движение корабля в рамках экрана"""
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x +=self.settings.ship_speed
         if self.moving_left and self.rect.left>0:
@@ -23,4 +30,10 @@ class Ship():
         self.rect.x = self.x
 
     def blitme(self):
+        """Отрисовка корабля на экране в настоящий момент времени"""
         self.screen.blit(self.image,self.rect)
+
+    def center_ship(self):
+        """Размещение корабля в середине нижней части экрана"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
