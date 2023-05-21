c9e824182833de3cb94232a4e572e2e6a7191f4f
Artamonov Anfisa <123888607+anfisartamonova@users.noreply.github.com>
2023-05-21 17:24:59 +0300

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,9 +1,13 @@
 import pygame.font
+from pygame.sprite import Group
+from ship import Ship
  
 class Scoreboard():
     """Вывод счета"""
     def __init__(self, ai_game):
         """Инициализация параметров для подсчета очков"""
+        
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -12,8 +16,20 @@ class Scoreboard():
         #настройки шрифта для вывода счета
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
-        #подготовка исходного изображения
+        #изображение счета, уровня, чило жизней
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
+
+    def prep_ships(self):
+        """Количество оставшихся кораблей."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
 
     def prep_score(self):
         """Преобразование текущего счета в картинку"""
@@ -26,5 +42,35 @@ class Scoreboard():
         self.score_rect.top = 20
 
     def show_score(self):
-        """Вывод счета на экран в верхний правый угол"""
+        """Вывод счета, рекорда, уровень и число жизней на экран в верхний правый угол"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
+
+    def prep_high_score(self):
+        """Преобразование рекордного счета в графическое изображение"""
+        high_score = self.stats.high_score
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color)
+
+        #выравнивание изображения оекорда по центру верхней стороны
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def check_high_score(self):
+        """Проверка, появился ли новый рекорд"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
+
+    def prep_level(self):
+        """Преобразование уровеня в графическое изображение"""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color)
+
+        #вывод уровня под текущим счетом
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
