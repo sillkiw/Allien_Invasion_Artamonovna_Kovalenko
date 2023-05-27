45b9de8d81c7988f35103a056a2f1f096183cc2c
AnatolYKovalenko <anatoly_kovalenko_03@mail.ru>
2023-05-27 06:14:01 +0300

    Update window.py

--- window.py
+++ window.py
@@ -18,7 +18,7 @@ class AlienInvasion:
         """Инициализация игры, создание окна"""    
         pg.init()
         pg.display.set_caption("Alien Invasion")
-
+       
         #полноэкранный режим
         self.screen = pg.display.set_mode((0,0),pg.FULLSCREEN)
         self.settings = Settings(self,self.screen.get_rect().width,self.screen.get_rect().height)
@@ -28,6 +28,7 @@ class AlienInvasion:
         self.bullets = pg.sprite.Group()
         self.aliens = pg.sprite.Group()
         self.allien_bullets = pg.sprite.Group()
+        self.len_block = 0
         self._create_fleet()
         self.images_explosion = []
         self.avaibshot = True
@@ -39,7 +40,7 @@ class AlienInvasion:
         #создание для хранения статистики и панели результатов
         self.stats = GameStats(self)
         self.sb = Scoreboard(self)
-
+       
         #создание кнопки Play
         self.play_button = Button(self, "Play")
 
@@ -52,7 +53,7 @@ class AlienInvasion:
                 self.ship.update()
                 self._update_bullets()
                 self._update_aliens()
-                if self.stats.level >= 1:
+                if self.stats.level >= 3:
                     self._alien_shoot()
                     self._update_allien_bullets()
                 self.update_lifetimes()
@@ -68,10 +69,11 @@ class AlienInvasion:
     def _ship_hit(self):
         """Обработка столкновения корабля с пришельцем"""
         #уменьшение ships_left - число оставшихся кораблей
-        if self.stats.ships_left > 1:
+        if self.stats.ships_left >= 0:
             #уменьшение ships_left и обновление панели счета
             self.stats.ships_left -= 1
-            self.sb.prep_lives()
+            self.sb.prep_lives()  
+            self.len_block = 0
 
             #очистка списков пришельцев и снарядов
             self.avaibshot = True
@@ -105,7 +107,9 @@ class AlienInvasion:
                 self._update_exp()
                 duration+=0.5
                 pg.time.delay(10)
-            
+            lose = pg.mixer.Sound("sounds/gameover.mp3")
+            lose.set_volume(0.5)
+            lose.play()
             self.start_new_level()
             self.stats.game_active = False
             pg.mouse.set_visible(True)
@@ -138,14 +142,15 @@ class AlienInvasion:
                 shotal[alien.rect.x] = alien
             elif alien.rect.y > shotal[alien.rect.x].rect.y:
                 shotal[alien.rect.x] = alien
-        if self.avaibshot and  len(self.allien_bullets) <2:
+        if self.avaibshot and  len(self.allien_bullets) < self.settings.allien_bullets_allowed:
             al1 = shotal.values()
-            al1 = random.choice(list(al1))  
-            explsound =  firesound = pg.mixer.Sound("sounds/alshot.mp3")
-            explsound.set_volume(0.2)
-            explsound.play()
-            self.avaibshot = False
-            self.allien_bullets.add(AllienBullet(self,al1))
+            if list(al1) != []:
+                al1 = random.choice(list(al1))  
+                explsound =  firesound = pg.mixer.Sound("sounds/alshot.mp3")
+                explsound.set_volume(0.2)
+                explsound.play()
+                self.avaibshot = False
+                self.allien_bullets.add(AllienBullet(self,al1))
             
 
     def _create_fleet(self):
@@ -190,10 +195,17 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
+        sd = pg.mixer.Sound("sounds/down.mp3")
+        sd.set_volume(0.5)
+        sd.play()
 
     def _create_alien(self,alien_number, row_number):
         """Создание пришельца с учетом его места"""
         alien = Alien(self)
+        if self.stats.level >= 5 and random.choice([True,False]) and self.len_block < self.settings.max_len_block:
+            alien.image = pg.transform.scale(pg.image.load('images/alienshbl.png').convert_alpha(),(80,80))
+            alien.isblock = True
+            self.len_block+=1
         alien_width,alien_height = alien.rect.size
         alien.x = alien_width+2*alien_width*alien_number 
         alien.rect.x = alien.x
@@ -217,7 +229,6 @@ class AlienInvasion:
             allien_bullet.draw_bullet()
         #отрисовка пришельца
         self.aliens.draw(self.screen)
-    
         #вывод счета
         self.sb.show_score()
 
@@ -250,20 +261,22 @@ class AlienInvasion:
     def _check_bullet_alien_collisions(self):
         """проверка попадания в пришельца и последующее уничтожение снаряда и пришельца"""
         #(True, True - нужно ли удалять снаряд пришельца и  соответственно)      
-        collisions = pg.sprite.groupcollide(self.bullets,self.aliens,True,True)
+        collisions = pg.sprite.groupcollide(self.bullets,self.aliens,True,False)
         #обновление счета
         if collisions:
-            self.stats.score += self.settings.alien_points
-         
             #1 пришелец += очки за него
             for aliens in collisions.values():
                 for alien in aliens:
-                    explsound =  firesound = pg.mixer.Sound("sounds/expl.mp3")
-                    explsound.set_volume(0.6)
-                    explsound.play()
-                    x = alien.rect.centerx + 20
-                    y = alien.rect.centery
-                    self.exps.add(ExplosionFX(self.screen, self.images_explosion, 1, x, y))
+                    if alien.isblock:
+                        alien.break_block()
+                    else:
+                        explsound =  firesound = pg.mixer.Sound("sounds/expl.mp3")
+                        explsound.set_volume(0.6)
+                        explsound.play()
+                        self.aliens.remove(alien)
+                        x = alien.rect.centerx + 20
+                        y = alien.rect.centery
+                        self.exps.add(ExplosionFX(self.screen, self.images_explosion, 1, x, y))
                 self.stats.score += self.settings.alien_points * len(aliens)
             #новая картинка счета
             self.sb.prep_score()
@@ -276,7 +289,11 @@ class AlienInvasion:
     def start_new_level(self):
         #есть ли еще пришельцы
         if not self.aliens:
-            
+            win = pg.mixer.Sound("sounds\win.mp3")
+            win.set_volume(0.5)
+            win.play()
+            self.len_block = 0
+          
         # Уничтожение существующих снарядов и создание нового флота.
             self.bullets.empty()
             self.allien_bullets.empty()
@@ -337,6 +354,7 @@ class AlienInvasion:
         self.aliens.empty()
         self.bullets.empty()
         self.allien_bullets.empty()
+        self.len_block = 0
             #создание нового флота и размещение корабля в центре
         self._create_fleet()
         self.ship.center_ship()
